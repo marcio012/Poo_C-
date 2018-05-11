20ec5dd4607fe3e88995144d4e4fd4d566fc4b91
marcio012 <marcio012@edu.unifor.br>
2018-05-11 12:01:04 -0300

    dados inline, parse de dados => int e double, array

---
+++ dadosInline/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace dadosInline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            int y; 
+            double z; 
+
+            // vetor de string 
+            string[] vet = Console.ReadLine().Split(' ');
+
+            x = vet[0]; 
+            y = int.Parse(vet[1]); // convert to int 
+            z = double.Parse(vet[2], CultureInfo.InvariantCulture); // convet to double
+
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+
+        }
+    }
+}
