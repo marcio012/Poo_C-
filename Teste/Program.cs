ced6be73e3f41582a07e33f3a5fd6383f3a21071
marcio012 <marcio012@edu.unifor.br>
2018-05-11 09:36:16 -0300

    inicio do projeto

---
+++ Teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace poo_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
+            // Console.ReadLine();
+        }
+    }
+}
