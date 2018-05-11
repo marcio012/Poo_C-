c20364671ded109eccf7a8d88328b2df771e15df
marcio012 <marcio012@edu.unifor.br>
2018-05-11 11:50:42 -0300

    ajuste do git ignore e novo app com aplicação dos tipos int

---
+++ app4-int/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace app4_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            int y;
+            double z;
+            char w;
+            System.Console.WriteLine();
+            x = Console.ReadLine();
+            y = int.Parse(Console.ReadLine());
+            z = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            w = char.Parse(Console.ReadLine());
+
+            System.Console.WriteLine(x);
+            System.Console.WriteLine(y);
+            System.Console.WriteLine(z);
+            System.Console.WriteLine(w);
+
+            System.Console.WriteLine();
+        }
+
+    }
+}
