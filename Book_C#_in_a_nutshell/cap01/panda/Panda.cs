59590dc2686a81bdccd33307cf3de4e217671d83
marcio012 <marcio012@edu.unifor.br>
2018-05-13 13:12:00 -0300

    book C# in NutShell exemplos para estudo do codigo

---
+++ Book_C#_in_a_nutshell/cap01/panda/Panda.cs
@@ -0,0 +1,12 @@
+public class Panda
+{
+    public string Nome;
+    public static int Population;
+
+    public Panda(string n)
+    {
+      Nome = n;
+      Population = Population + 1;
+    }
+
+}
---
+++ Book_C#_in_a_nutshell/cap01/panda/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace panda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Panda p1 = new Panda("Pan Dee");
+            Panda p2 = new Panda("Pan Dah");
+
+            Console.WriteLine(p1.Nome);
+            Console.WriteLine(p2.Nome);
+
+            Console.WriteLine(Panda.Population);
+        }
+    }
+}
