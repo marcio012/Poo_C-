152edab670ea1901960400cc73d297aef8d6d9c2
marcio012 <marcio012@edu.unifor.br>
2018-05-13 10:43:29 -0300

    estrutura de repeticao for

---
+++ estrutura_for/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace estrutura_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N; 
+            double x, y, resultado;
+            Console.WriteLine("Informe dois numéros distintos");
+            N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                x = double.Parse(vet[0], CultureInfo.InvariantCulture);
+                y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+                if (y == 0.0)
+                {
+                    Console.WriteLine("não existe");
+                } else {
+                    resultado = x/y;
+                    Console.WriteLine(resultado.ToString("F3", CultureInfo.InstalledUICulture));
+                }
+            
+            }
+            Console.ReadLine();
+        }
+    }
+}
