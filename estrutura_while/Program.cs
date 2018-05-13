5c1f8d22d661fafb70147f32193184520f5f6983
marcio012 <marcio012@edu.unifor.br>
2018-05-13 00:02:29 -0300

    estrutura_while

---
+++ estrutura_while/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace estrutura_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade, soma, cont;
+            double media;
+
+            idade = 0;
+            soma = 0;
+            cont = 0;
+
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 0) {
+
+                while(idade >= 0) {
+                    // idade = int.Parse(Console.ReadLine());
+                    soma = soma + idade;
+                    cont = cont + 1;
+                    idade = int.Parse(Console.ReadLine());
+                }
+                
+                media = (double) soma / cont; 
+                Console.WriteLine(media.ToString("F2", CultureInfo.InvariantCulture));
+
+            } else {
+                Console.WriteLine("Impossivel Calcular.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
