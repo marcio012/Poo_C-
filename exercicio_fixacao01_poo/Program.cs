d3aa0207f1c0322a3c88d3d2749116598e5bfb9b
marcio012 <marcio012@edu.unifor.br>
2018-05-16 14:49:31 -0300

    inicio do exercicios de fixação da udemy

---
+++ exercicio_fixacao01_poo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicio_fixacao01_poo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
