b83a24c84f9c99c54b5a51fe03e5d66abcb1ab71
marcio012 <marcio012@edu.unifor.br>
2018-05-17 06:30:51 -0300

    inicio do exercicios de fixação

---
+++ exercicio_fixacao01_poo/Banco.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace exercicio_fixacao01_poo
+{
+    class Banco
+    {
+        public int numero { get; private set; }
+        public string nomeTitular { get; set; }
+        public double saldoConta { get; set; }
+    }
+}
